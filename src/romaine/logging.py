d5b670b5c0aeebc4739529b58aca0fc42e3097ed
Alistair Broomhead <alistair.broomhead@gmail.com>
2016-01-20 11:30:50 +0000

    Implement the very basics of logging, with extra tests

--- src/romaine/logging.py
+++ src/romaine/logging.py
@@ -2,13 +2,18 @@
 
 class RomaineLogger(object):
 
+    INFO = object()
+    WARNING = object()
     ERROR = object()
 
     def __enter__(self):
         pass
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+        if isinstance(exc_val, Exception):
+            self.alert(self.INFO, exc_val)
+            self.alert(self.ERROR, exc_val)
+            return True
 
     def alert(self, level, body):
         pass
--- tests/test_logging_api.py
+++ tests/test_logging_api.py
@@ -30,10 +30,19 @@ from romaine import exc
 
 class TestLoggingAPI(unittest.TestCase):
 
-    # Todo - test that UnimplementedStepException contains step info
+    @mock.patch('romaine.logging.RomaineLogger.alert')
+    def test_nothing_logged(self, mock_alert):
+
+        # Given a logger context
+        with logging.RomaineLogger():
+            # When I don't use the logger
+            pass
+        # Then the logger does not alert
+        assert not mock_alert.called
 
     @mock.patch('romaine.logging.RomaineLogger.alert')
     def test_unimplemented_step_error(self, mock_alert):
+        # Todo - test that UnimplementedStepException contains step info
 
         # Given a logger context
         with logging.RomaineLogger():
@@ -42,19 +51,29 @@ class TestLoggingAPI(unittest.TestCase):
         # Then the logger alerts with an error
         assert mock_alert.called
 
-        for (obj, level, body) in mock_alert.call_args_list:
+        for (args, kwargs) in mock_alert.call_args_list:
+            level, body = args
             if level is logging.RomaineLogger.ERROR:
                 break
         else:
             raise RuntimeError("No error found in alert calls!")
 
-    def test_unimplemented_step_stub(self):
+    @mock.patch('romaine.logging.RomaineLogger.alert')
+    def test_unimplemented_step_stub(self, mock_alert):
 
         # Todo - work out what we want to do with this info
 
         # Given a logger context
-        # When I raise an error for a missing feature step implementation
+        with logging.RomaineLogger():
+            # When I raise an error for a missing feature step implementation
+            raise exc.UnimplementedStepError
         # Then the logger alerts with information
+        assert mock_alert.called
 
-        raise NotImplementedError
+        for (args, kwargs) in mock_alert.call_args_list:
+            level, body = args
+            if level is logging.RomaineLogger.INFO:
+                break
+        else:
+            raise RuntimeError("No info found in alert calls!")
 
