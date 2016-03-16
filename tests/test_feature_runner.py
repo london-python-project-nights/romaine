90226b0565965fba890db85cb9afc5334a7a7ea6
Alistair Broomhead <alistair.broomhead@gmail.com>
2016-03-16 12:43:17 +0000

    Started feature runner

---
+++ tests/test_feature_runner.py
@@ -0,0 +1,21 @@
+"""
+Take a feature object, retrieve steps and run them all
+"""
+import importlib
+import unittest
+
+
+class TestFeatureRunner(unittest.TestCase):
+
+    def test_full(self):
+        # Given I have Romaine's core
+        from romaine.core import Core
+        core = Core()
+        # And I have some step definitions
+        importlib.import_module("test_data.steps.some_steps")
+        # And I have a feature object
+        feature = {}
+        # When I run the feature
+        statistics = core.run_feature(feature)
+        # Then the output shows that the expected steps have been run
+        assert statistics == {}
