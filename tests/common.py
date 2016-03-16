97539f3fb2b4477d57ffd1e7c5e8733947564025
Alistair Broomhead <alistair.broomhead@gmail.com>
2016-03-16 12:57:17 +0000

    Ensure that tests which import test_data do not interact

--- tests/common.py
+++ tests/common.py
@@ -10,6 +10,15 @@ sys.path.append(package_directory)
 import romaine
 
 
+def unload_test_data():
+    keys = tuple(
+        key for key in sys.modules
+        if key.startswith("test_data")
+    )
+    for key in keys:
+        sys.modules.pop(key)
+
+
 # Utility for getting an initialised parser.
 def get_romaine_parser():
     return romaine.Core().Parser()
